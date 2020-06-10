e22cf4887fb2fd025e51fa42cc9193bb18223603
Belianin <belyaninwork@gmail.com>
2020-06-10 22:11:59 +0500

    Preloaded images and clean-up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Carousel from "react-material-ui-carousel";
-import Paper from '@material-ui/core/Paper'
 
-class App extends React.Component {
-  render () {
+import './modules.d.ts';
+// @ts-ignore
+import PreloadImage from "react-preload-image";
+
+const imageLinks = [
+    "https://www.instagram.com/p/CBLf4uzjGX5/media?size=l",
+    "https://www.instagram.com/p/CBDhNaijfsX/media?size=l",
+    "https://www.instagram.com/p/CBC6buMjiNK/media?size=l",
+    "https://www.instagram.com/p/CBBeCCujJ5y/media?size=l",
+    "https://www.instagram.com/p/CBA_jXcDApF/media?size=l",
+    "https://www.instagram.com/p/CA-fCC0DltY/media?size=l",
+    "https://www.instagram.com/p/CA9xwXzjBDt/media?size=l",
+    "https://www.instagram.com/p/CA8YuyjDr-7/media?size=l"
+];
+
+interface IAppProps {
+
+}
+
+interface IAppState {
+    images: HTMLImageElement[]
+}
+
+class App extends React.Component<IAppProps, IAppState> {
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            images: []
+        }
+    }
+
+    render () {
     return (
         <div>
           <header>
@@ -23,37 +52,14 @@ class App extends React.Component {
               </div>
             </div>
             <div className={"gallery-container"}>
-              <Carousel autoPlay={true} indicators={false} navButtonsAlwaysVisible={true} className={"gallery"} fullHeightHover={false}>
-                {
-                  [
-                      "https://www.instagram.com/p/CBLf4uzjGX5/media?size=l",
-                      "https://www.instagram.com/p/CBDhNaijfsX/media?size=l",
-                      "https://www.instagram.com/p/CBC6buMjiNK/media?size=l",
-                      "https://www.instagram.com/p/CBBeCCujJ5y/media?size=l",
-                      "https://www.instagram.com/p/CBA_jXcDApF/media?size=l",
-                      "https://www.instagram.com/p/CA-fCC0DltY/media?size=l",
-                      "https://www.instagram.com/p/CA9xwXzjBDt/media?size=l",
-                      "https://www.instagram.com/p/CA8YuyjDr-7/media?size=l"
-                  ].map(link => {
-                    return <img src={link} className={"gallery-img"}/>
-                  })
-                }
+              <Carousel autoPlay={true} indicators={false} navButtonsAlwaysVisible={true} interval={4000} animation={"fade"} className={"gallery"} fullHeightHover={false} timeout={200}>
+                {imageLinks.map(i => (<PreloadImage className={"gallery-img"} src={i} />))}
             </Carousel>
           </div>
           </main>
           <footer>
             <p className="footer-content">Телефон: <a href="tel:+1234567890">+1234567890</a></p>
           </footer>
-
-          <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-                  integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-                  crossOrigin="anonymous"></script>
-          <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-                  integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-                  crossOrigin="anonymous"></script>
-          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
-                  integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
-                  crossOrigin="anonymous"></script>
         </div>
     );
   }
